2179695e99625bd94f6170307992dbf03627060c
kurehajime <xiidec@gmail.com>
2021-11-08 11:34:58 +0000

    update ranks

--- app/controllers/ranks_controller.rb
+++ app/controllers/ranks_controller.rb
@@ -1,2 +1,13 @@
 class RanksController < ApplicationController
+    include Github
+
+    def index
+        @contributors = []
+        @title = params[:title]
+        if params[:repo] 
+            user = params[:repo].split("/")[0]
+            repo = params[:repo].split("/")[1]
+            @contributors =  get_stats_contributors_ago(user,repo,0)
+        end
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   get "make" => "make#index"
+  get "ranks" => "ranks#index"
 end
